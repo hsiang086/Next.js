ed379d1a4466588ea30d3ffc772c6c6298d86de8
hsiang086 <fabiowu20070806@gmail.com>
2023-09-02 12:12:36 +0800

    added setup form 🚀

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,6 +1,7 @@
 import { redirect } from "next/navigation";
 import { initialProfile } from "@/lib/initial-profile";
 import { db } from "@/lib/db";
+import InitialModel from "@/components/models/Initial-model";
 
 const SetupPage = async () => {
     const profile = await initialProfile();
@@ -19,9 +20,7 @@ const SetupPage = async () => {
     }
 
     return (  
-        <div>
-            Create a server
-        </div>
+        <InitialModel />
     );
 }
  
